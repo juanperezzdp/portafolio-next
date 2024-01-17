c52a217eb0bd3c8949a68784b54218aa82704de3
juan perez <juanperezzdp@gmail.com>
2024-01-17 18:44:33 -0500

    form validation and npm react-hook-form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
-  const [email, setEmail] = useState("");
-  const [issue, setIssue] = useState("");
-  const [transition, setTransition] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (data) => {
     try {
       const response = await fetch("/api/send", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          email: email,
-          issue: issue,
-        }),
+        body: JSON.stringify(data),
       });
 
       if (response.ok) {
@@ -40,49 +38,50 @@ const Contact = () => {
         <div className="w-64 sm:w-80 h-[25rem] absolute">
           <div
             className={`${
-              transition ? "jumpp" : ""
+              true ? "jumpp" : ""
             } absolute -left-12 -top-12 h-28 w-28 sm:h-40 sm:w-40 rounded-full bg-gradient-to-br from-violet-700 to-teal-500`}
           ></div>
 
           <div
             className={`${
-              transition ? "jump" : ""
+              true ? "jump" : ""
             } absolute transition-all duration-500 -bottom-12 -right-12 h-28 w-28 sm:h-40 sm:w-40 rounded-full bg-gradient-to-r from-fuchsia-700 to-blue-600`}
           ></div>
           <form
-            onSubmit={handleSubmit}
-            className="gap-4 p-4 flex flex-col justify-center items-center  text-white shadow-form w-72 sm:w-80 h-[28rem]  bg-opacity-10 bg-white absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-2xl backdrop-blur-md border-2 border-black border-opacity-10 p-50 "
+            onSubmit={handleSubmit(onSubmit)}
+            className="gap-1 p-4 flex flex-col justify-center items-center  text-white shadow-form w-72 sm:w-80 h-[28rem]  bg-opacity-10 bg-white absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-2xl backdrop-blur-md border-2 border-black border-opacity-10 p-50 "
           >
             <h3 className="text-2xl">Contact Me</h3>
             <div className="flex flex-col">
               <label htmlFor="email">Email</label>
               <input
-                onClick={() => {
-                  setTransition(true);
-                }}
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                })}
                 className="focus:bg-white focus:bg-opacity-40 w-[14.5rem] appearance-none placeholder-white resize-none focus:outline-none rounded-lg p-2 bg-opacity-40 bg-white"
                 placeholder="example@gmail.com"
-                type="email"
-                name="email"
-                value={email}
-                required={true}
-                onChange={(e) => setEmail(e.target.value)}
               />
+              <div className="w-full h-6 text-sm">
+                {errors.email && (
+                  <p className="text-red-500">{errors.email.message}</p>
+                )}
+              </div>
             </div>
 
             <div className="flex flex-col">
               <label htmlFor="issue">Issues</label>
               <textarea
-                onClick={() => {
-                  setTransition(true);
-                }}
-                required={true}
-                className="placeholder-white resize-none focus:outline-none rounded-lg p-2 h-48 w-[14.5rem] bg-opacity-40 bg-white"
+                {...register("issue", { required: "Issue is required" })}
+                className="h-40 placeholder-white resize-none focus:outline-none rounded-lg p-2 w-[14.5rem] bg-opacity-40 bg-white"
                 placeholder="Write to the subject"
-                name="issue"
-                value={issue}
-                onChange={(e) => setIssue(e.target.value)}
               ></textarea>
+
+              <div className="w-full h-6 text-sm">
+                {errors.issue && (
+                  <p className="text-red-500">{errors.issue.message}</p>
+                )}
+              </div>
             </div>
             <button
               type="submit"
