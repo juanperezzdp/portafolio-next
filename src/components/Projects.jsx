be50945bc5cbf7cbc463bd008731c2b10479c518
juan perez <juanperezzdp@gmail.com>
2024-04-24 19:05:36 -0500

    delete projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -139,61 +139,6 @@ const Projects = () => {
           </div>
           <Image width={500} src={UiLinearGradients} alt="img-projects" />
         </div>
-        <div className="flex flex-col sm:flex-row justify-center items-center">
-          <Image width={500} src={ArtPx} alt="img-projects" />
-          <div className="text-white text-balance text-center sm:text-left sm:w-[30rem]">
-            <p>
-              ArtPx is a free library of images and videos created with React.
-              It provides high-quality search results in various categories and
-              is easy to use. Ideal for your visual and design search needs.
-            </p>
-            <p className="mt-4">Technology and tools used in the project:</p>
-            <div className=" w-full flex justify-center sm:justify-start">
-              <RiJavascriptFill className=" text-yellow-400 text-2xl" />
-              <FaReact className=" text-blue-700 text-2xl" />
-              <SiPixabay className=" text-green-500 text-2xl" />
-              <FaSass className=" text-pink-400 text-2xl" />
-            </div>
-
-            <div className="mt-4 cursor-pointer">
-              <Link
-                target="_blank"
-                href="https://lovely-pastelito-3ba768.netlify.app/"
-                className="z-30 inline-flex background-shine bg-[linear-gradient(110deg,#939393,45%,#232323,55%,#939393)] hover:bg-[linear-gradient(110deg,#4a40ae,45%,#ffff,55%,#4a40ae)] bg-[length:250%_100%] bg-clip-text text-xl text-transparent"
-              >
-                View website
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center">
-          <div className="text-white text-balance text-center sm:text-right sm:w-[30rem]">
-            <p className="sm:text-right">
-              CoinTz is a responsive web application developed with React,
-              utilizing the CoinGecko API to display current cryptocurrency
-              prices.
-            </p>
-            <p className="sm:text-right">It is developed with ReactJs.</p>
-            <p className="mt-4">Technology and tools used in the project:</p>
-            <div className=" w-full flex justify-center sm:justify-end">
-              <RiJavascriptFill className=" text-yellow-400 text-2xl" />
-              <FaReact className=" text-blue-700 text-2xl" />
-              <FaSass className=" text-pink-400 text-2xl" />
-              <FaBitcoin className=" text-yellow-600 text-2xl" />
-            </div>
-
-            <div className="mt-4 cursor-pointer sm:text-right">
-              <Link
-                target="_blank"
-                href="https://resilient-fairy-8d17a5.netlify.app/"
-                className=" inline-flex background-shine bg-[linear-gradient(110deg,#939393,45%,#232323,55%,#939393)] hover:bg-[linear-gradient(110deg,#4a40ae,45%,#ffff,55%,#4a40ae)] bg-[length:250%_100%] bg-clip-text text-xl text-transparent"
-              >
-                View website
-              </Link>
-            </div>
-          </div>
-          <Image width={500} src={CoinTz} alt="img-projects" />
-        </div>
       </div>
     </section>
   );
